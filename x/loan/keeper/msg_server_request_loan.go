7b32439f547711d8397d474d011398ac84c1f401
kaka <hikarutakahashi0523@gmail.com>
2022-11-25 10:12:00 +0900

    Add request-loan message

--- x/loan/keeper/msg_server_request_loan.go
+++ x/loan/keeper/msg_server_request_loan.go
@@ -3,15 +3,45 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"loan/x/loan/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) RequestLoan(goCtx context.Context, msg *types.MsgRequestLoan) (*types.MsgRequestLoanResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	//新しいloanを作成
+	var loan = types.Loan{
+		Amount:     msg.Amount,
+		Fee:        msg.Fee,
+		Collateral: msg.Collateral,
+		Deadline:   msg.Deadline,
+		State:      "requested",
+		Borrower:   msg.Creator,
+	}
 
+	//担保は金額（+手数料？）以上であること
+
+	//Creator(借り手)をsdk.AccAddressに変換
+	borrower, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+
+	//Collateralをトークンに変換
+	collateral, err := sdk.ParseCoinsNormalized(loan.Collateral)
+	if err != nil {
+		panic(err)
+	}
+
+	//モジュールアカウントにトークンを送信する
+	sdkError := k.bankKeeper.SendCoinsFromAccountToModule(ctx, borrower, types.ModuleName, collateral)
+	if sdkError != nil {
+		return nil, sdkError
+	}
+
+	//loanストアに書き込む(新規登録)
+	k.AppendLoan(ctx, loan)
 	return &types.MsgRequestLoanResponse{}, nil
 }
