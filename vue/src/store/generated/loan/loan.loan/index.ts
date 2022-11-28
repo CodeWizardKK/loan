f3903f1288287d755ff44d3b4989b69b091e1620
kaka <hikarutakahashi0523@gmail.com>
2022-11-28 15:34:03 +0900

    Add cancel-loan message

--- vue/src/store/generated/loan/loan.loan/index.ts
+++ vue/src/store/generated/loan/loan.loan/index.ts
@@ -199,6 +199,21 @@ export default {
 		},
 		
 		
+		async sendMsgLiquidateLoan({ rootGetters }, { value, fee = [], memo = '' }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgLiquidateLoan(value)
+				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
+	gas: "200000" }, memo})
+				return result
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgLiquidateLoan:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgLiquidateLoan:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
 		async sendMsgRequestLoan({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -211,21 +226,6 @@ export default {
 					throw new Error('TxClient:MsgRequestLoan:Init Could not initialize signing client. Wallet is required.')
 				}else{
 					throw new Error('TxClient:MsgRequestLoan:Send Could not broadcast Tx: '+ e.message)
-				}
-			}
-		},
-		async sendMsgRepayLoan({ rootGetters }, { value, fee = [], memo = '' }) {
-			try {
-				const txClient=await initTxClient(rootGetters)
-				const msg = await txClient.msgRepayLoan(value)
-				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
-	gas: "200000" }, memo})
-				return result
-			} catch (e) {
-				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgRepayLoan:Init Could not initialize signing client. Wallet is required.')
-				}else{
-					throw new Error('TxClient:MsgRepayLoan:Send Could not broadcast Tx: '+ e.message)
 				}
 			}
 		},
@@ -244,6 +244,21 @@ export default {
 				}
 			}
 		},
+		async sendMsgRepayLoan({ rootGetters }, { value, fee = [], memo = '' }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgRepayLoan(value)
+				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
+	gas: "200000" }, memo})
+				return result
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgRepayLoan:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgRepayLoan:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
 		async sendMsgCancelLoan({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -259,22 +274,20 @@ export default {
 				}
 			}
 		},
-		async sendMsgLiquidateLoan({ rootGetters }, { value, fee = [], memo = '' }) {
+		
+		async MsgLiquidateLoan({ rootGetters }, { value }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
 				const msg = await txClient.msgLiquidateLoan(value)
-				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
-	gas: "200000" }, memo})
-				return result
+				return msg
 			} catch (e) {
 				if (e == MissingWalletError) {
 					throw new Error('TxClient:MsgLiquidateLoan:Init Could not initialize signing client. Wallet is required.')
-				}else{
-					throw new Error('TxClient:MsgLiquidateLoan:Send Could not broadcast Tx: '+ e.message)
+				} else{
+					throw new Error('TxClient:MsgLiquidateLoan:Create Could not create message: ' + e.message)
 				}
 			}
 		},
-		
 		async MsgRequestLoan({ rootGetters }, { value }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -285,19 +298,6 @@ export default {
 					throw new Error('TxClient:MsgRequestLoan:Init Could not initialize signing client. Wallet is required.')
 				} else{
 					throw new Error('TxClient:MsgRequestLoan:Create Could not create message: ' + e.message)
-				}
-			}
-		},
-		async MsgRepayLoan({ rootGetters }, { value }) {
-			try {
-				const txClient=await initTxClient(rootGetters)
-				const msg = await txClient.msgRepayLoan(value)
-				return msg
-			} catch (e) {
-				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgRepayLoan:Init Could not initialize signing client. Wallet is required.')
-				} else{
-					throw new Error('TxClient:MsgRepayLoan:Create Could not create message: ' + e.message)
 				}
 			}
 		},
@@ -314,6 +314,19 @@ export default {
 				}
 			}
 		},
+		async MsgRepayLoan({ rootGetters }, { value }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgRepayLoan(value)
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgRepayLoan:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgRepayLoan:Create Could not create message: ' + e.message)
+				}
+			}
+		},
 		async MsgCancelLoan({ rootGetters }, { value }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -324,19 +337,6 @@ export default {
 					throw new Error('TxClient:MsgCancelLoan:Init Could not initialize signing client. Wallet is required.')
 				} else{
 					throw new Error('TxClient:MsgCancelLoan:Create Could not create message: ' + e.message)
-				}
-			}
-		},
-		async MsgLiquidateLoan({ rootGetters }, { value }) {
-			try {
-				const txClient=await initTxClient(rootGetters)
-				const msg = await txClient.msgLiquidateLoan(value)
-				return msg
-			} catch (e) {
-				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgLiquidateLoan:Init Could not initialize signing client. Wallet is required.')
-				} else{
-					throw new Error('TxClient:MsgLiquidateLoan:Create Could not create message: ' + e.message)
 				}
 			}
 		},
--- x/loan/keeper/msg_server_cancel_loan.go
+++ x/loan/keeper/msg_server_cancel_loan.go
@@ -2,16 +2,48 @@ package keeper
 
 import (
 	"context"
+	"fmt"
+
+	"loan/x/loan/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"loan/x/loan/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) CancelLoan(goCtx context.Context, msg *types.MsgCancelLoan) (*types.MsgCancelLoanResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	//特定のloanを取得する
+	loan, isFound := k.GetLoan(ctx, msg.Id)
+	if !isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
+	}
+
+	//キャンセルできるのは本人だけ
+	if loan.Borrower != msg.Creator {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Cannot cancel: not the borrower")
+	}
+
+	//特定のloanのステータスが要求済みか確認する
+	if loan.State != "requested" {
+		return nil, sdkerrors.Wrapf(types.ErrWrongLoanState, "%v", loan.State)
+	}
+
+	//型変換
+	borrower, _ := sdk.AccAddressFromBech32(loan.Borrower)
+	collateral, _ := sdk.ParseCoinsNormalized(loan.Collateral)
+
+	//モジュールアカウント(エスクローアカウントとして使用)から借り手へ担保(collateral)を返す。
+	err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, borrower, collateral)
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrWrongLoanState, "Cannot send coins")
+	}
+
+	//ローンがキャンセルされたので、ローン情報を更新する
+	loan.State = "cancelled"
+
+	//loanストア更新
+	k.SetLoan(ctx, loan)
 
 	return &types.MsgCancelLoanResponse{}, nil
 }
